6fe3f67ab9603a2200b9a40623b548c0d026858b
Francisco Branco <franciscomcb782@gmail.com>
2023-06-07 16:48:00 +0100

    backend

--- db/index.js
+++ db/index.js
@@ -5,8 +5,7 @@ const mongoose = require("mongoose");
 // ℹ️ Sets the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
-const MONGO_URI =
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/watchVerse_BackEnd";
+const MONGO_URI = `mongodb+srv://chikifella:M4HGT5hlPtkBAhCW@watchverse.huywe3p.mongodb.net/?retryWrites=true&w=majority`;
 
 mongoose
   .connect(MONGO_URI)
